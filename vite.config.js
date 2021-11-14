3d2e2ffc90fed4b1e6210fd2fdd32dbfc6824f6e
joe <joe.chang1014@gmail.com>
2021-11-14 16:25:57 +0800

    fix vite  base config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig((mode) => {
-console.log("mode--", mode);
+console.log("mode--", mode.mode);
 console.log("prrr", process.env.VITE_BASE_URL);
 return {
-  base: mode.mode === "production" ? "./" : "/",
+  base: mode.mode === "prd" ? "./" : "/",
   plugins: [vue()],
   build: {
     outDir: './docs'
